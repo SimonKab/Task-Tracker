977f5282b9c1a24c2588f2631e53473e2ec0ac18
Simon Kab <sam24free@gmail.com>
2018-06-04 17:20:19 +0300

    Console and core setup scripts where changed: core setup script now search packages only in tasktracker_core folder, console script find path to parser and launch it correctly

--- setup_console.py
+++ setup_console.py
@@ -5,11 +5,11 @@ setup(
     name='tasktracker_console',
     version='1.0',
     description='something unuseful',
-    packages=find_packages(),
+    packages=['tasktracker_console'],
     include_package_data=False,
     install_requires=['peewee'],
     entry_points={
         'console_scripts':
-            ['tt = .start:run']
+            ['tt = tasktracker_console.console_parser:parse']
     }
 )
--- setup_core.py
+++ setup_core.py
@@ -1,12 +1,14 @@
 from setuptools import setup, find_packages
 from os.path import join, dirname
 
+root = 'tasktracker_core'
+packages = [root] + [(root + '.' + subfolder) for subfolder in find_packages(root)]
 
 setup(
     name='tasktracker_core',
     version='1.0',
     description='Core library for tasktracker',
-    packages=find_packages(),
+    packages=packages,
     include_package_data=False,
     install_requires=['peewee']
 )
