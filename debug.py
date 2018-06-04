a47d4f10966690b20eaea7a67b9cf0f556c9d105
Simon Kab <sam24free@gmail.com>
2018-06-04 14:11:27 +0300

    Excess and temporary files where removed from git cache. gitignore file was restructed. Files in root are in the first group, folders are in the second group

--- debug.py
+++
@@ -1,6 +0,0 @@
-from tasktracker_server.storage.sqlite_peewee_adapters import ProjectStorageAdapter
-
-adapter = ProjectStorageAdapter()
-relations = adapter._get_all_relations()
-for relation in relations:
-    print(relation.__data__)
