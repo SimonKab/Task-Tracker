7d15b3459a501a8aaa27128d3aed6c5188230b78
Simon Kab <sam24free@gmail.com>
2018-06-07 09:46:28 +0300

    Documentation for utils module

--- tasktracker_core/utils.py
+++ tasktracker_core/utils.py
@@ -1,3 +1,18 @@
+'''Provide simple and routine functionality
+
+All functionality presented:
+Converting datetime to milliseconds
+Shifting datetime
+Creating time range with length of day
+Retrieve today datetime with 0 hour and 0 minutes
+Retrieve now datetime withoud microseconds
+
+Parse string representation of datetime
+
+Retrieve home folder path
+Creating file with specified path
+'''
+
 import datetime
 import re
 from pathlib import Path
@@ -13,6 +28,7 @@ def datetime_to_milliseconds(datetime_inst):
     return int((datetime_inst - epoch).total_seconds() * 1000.0)
 
 def create_shift(delta):
+    '''Returns start of epoch + delta'''
     return datetime.datetime.utcfromtimestamp(0) + delta
 
 def create_shift_in_millis(delta):
@@ -22,17 +38,21 @@ def shift_datetime_in_millis(datetime, delta):
     return datetime_to_milliseconds(datetime + delta)
 
 def get_time_range_of_day(datetime_inst):
+    '''For datetime 15.06.2018 20:40 returns (15.06.2018 0:00, 16.06.2018 0:00)'''
     start_day = datetime.datetime(datetime_inst.year, datetime_inst.month, datetime_inst.day)
     end_day = start_day + datetime.timedelta(days=1)
     return (start_day, end_day)
 
 def today():
+    '''For datetime 15.06.2018 20:40 returns 15.06.2018 0:00''' 
     return datetime.datetime.combine(datetime.date.today(), datetime.datetime.min.time())
 
 def now():
+    '''Current time with microseconds set to 0''' 
     return datetime.datetime.now().replace(microsecond=0)
 
 def parse_date(arg, relative_support=True):
+    '''Parse date like [0-9]-[0-9]-[0-9] and today[+-][0-9]'''
     simple_date = re.match('[0-9]+-[0-9]+-[0-9]+', arg)
     if simple_date is not None:
         return datetime.datetime.strptime(arg, "%d-%m-%Y")
@@ -53,6 +73,7 @@ def parse_date(arg, relative_support=True):
     return None
 
 def parse_time(arg, relative_support=True):
+    '''Parse date like [0-9]:[0-9] and now[+-][0-9]'''
     simple_time = re.match('[0-9]+:[0-9]+', arg)
     if simple_time is not None:
         return datetime.datetime.strptime(arg, "%H:%M").time()
@@ -74,10 +95,12 @@ def parse_time(arg, relative_support=True):
     return None
 
 def get_home_folder():
+    '''Returns home folder of library in Linux system only: $HOME/tasktracker''' 
     if platform.system() == 'Linux':
         return path.join(str(Path.home()), 'tasktracker')
 
 def get_file_in_home_folder(file_name):
+    '''Joins home folder with specified file name'''
     return path.join(get_home_folder(), file_name)
 
 def create_file_if_not_exists(file_path):
