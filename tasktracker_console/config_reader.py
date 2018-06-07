e917975d3f0f4a8d3c7f6ac026d150b171bd4bde
Simon Kab <sam24free@gmail.com>
2018-06-07 09:29:53 +0300

    Documentation for logging of core and configuration reader of console

--- tasktracker_console/config_reader.py
+++ tasktracker_console/config_reader.py
@@ -1,9 +1,44 @@
+'''Configuration reader of console application
+
+Configuration file is a file like INI in Windows. 
+It means markup rules specified by standard Python library "configparser"
+
+There are three groups: user, database and logger
+
+In user group there is only one item: name. It is name of primary user.
+All operations will be performed on its behalf
+
+In database group there is only one item: path. It is path to database file
+
+In logger group there are seven items:
+enable_logging - available or not logging
+enable_high_logging - available or not high logging in default logger
+enable_low_logging - available or not low logging in default logger
+high_log_path - path to high log file
+low_log_path - path to low log file
+high_log_level - level of high logging (see logging module)
+low_log_level - level of low_logging (see logging module)
+
+All read information stored in ConfigReader class
+'''
+
 import configparser
 from os import path
 
 from tasktracker_core import utils
 
 class ConfigReader():
+    '''Reads configuration file
+
+    You can set configuration file to proccess by config_path field
+    By default config file will be searched in home directory 
+    which core library specifies
+
+    Object of class stores information aboud configuration of app
+
+    read_config method reads and fills object with config information
+    write_username_in_config methos updates primary user name
+    '''
 
     DEFAULT_CONFIG_PATH = utils.get_file_in_home_folder('tm.conf')
 
@@ -21,6 +56,11 @@ class ConfigReader():
         self.enable_low_logging = None
 
     def read_config(self):
+        '''Reads and fills object with config information
+
+        Returns config object of configparser.ConfigParser. 
+        You can use it to retrieve data that ConfigReader can not retrieve
+        '''
         config = configparser.ConfigParser()
 
         try:
@@ -50,6 +90,7 @@ class ConfigReader():
         return config
 
     def write_username_in_config(self, username):
+        '''Write current login user name to config file'''
         config = self.read_config()
         if config is None:
             config = configparser.ConfigParser()
--- tasktracker_core/logging.py
+++ tasktracker_core/logging.py
@@ -1,3 +1,20 @@
+'''Logging behaviour of core
+
+Allow to customize default logger or initialize fully custom logger
+
+Module defines root logger for core with name "core". All other loggers
+will be children of this root logger. Library creates a lot of 
+subloggers with different names for different tasks. Existing of root logger
+allows to configure only one logger
+
+You can use default logger with specific params or initialize your
+specific logger
+
+LoggerConfig class manage root logger of package
+
+Use get_logger function with specific logger name to get sublogger of root logger
+'''
+
 import logging
 import logging.config
 import os
@@ -5,6 +22,45 @@ import os
 from tasktracker_core import utils
 
 class LoggerConfig():
+    '''Manage root logger of package
+
+    Default root logger separate message in two groups: low and high
+    Low group is not important, regular information that usually used for
+    debug and info purposes. By default messages with level INFO and lower
+    is transfered in low group
+    High group is important and very important messages like errors and warnings
+    By default messages with levels WARNING and higher is transfere in high group
+    Division into low and high allow to separate primary messages from others
+    that allow you to see the most important first
+    Low group is recorded in low.log in home directory by default
+    High group is recorded in high.log in home directory by default
+    Default logger consider all types of messages
+
+    You can change this params by specifying levels and paths of loggers.
+
+    high_log_path - path to high group
+    low_log_path - path to low group
+
+    high_log_level - level from which messages will be treated as high group
+    low_log_level - level to which messages will be treated as low group
+
+    Also you can disable either high or low group
+    Set high_logging_enabled to False to disable high logging
+    Set low_logging_enabled to False to disable low logging
+
+    To disable logging at all set logging_enabled to False
+    This field will block logging even in fully custom root logger
+
+    If you want to set your own logger with model of logging different 
+    from default, init logger with name 'core' and set custom_logger_enabled to True
+    In this case function get_core_root_logger will not try to specify default
+    behaviour of logger, saving you custom behaviour
+    If you want to change some specific params of default logger you can call
+    get_core_root_logger and change params in returned logger
+    To roll back logger to default settings just set custom_logger_enabled to False
+    and logger will be setted to default with next call of get_core_root_logger
+    '''
+
 
     DEFAULT_PATH_HIGH = utils.get_file_in_home_folder('high.log')
     DEFAULT_PATH_LOW = utils.get_file_in_home_folder('low.log')
@@ -12,6 +68,7 @@ class LoggerConfig():
     logging_enabled = True
     high_logging_enabled = True
     low_logging_enabled = True
+
     custom_logger_enabled = False
 
     high_log_path = DEFAULT_PATH_HIGH
@@ -29,7 +86,7 @@ class LoggerConfig():
             return record.levelno >= LoggerConfig.high_log_level
 
     @classmethod
-    def init_default_logger(cls, logger):
+    def _init_default_logger(cls, logger):
         utils.create_file_if_not_exists(cls.high_log_path)
         utils.create_file_if_not_exists(cls.low_log_path)
 
@@ -53,6 +110,12 @@ class LoggerConfig():
 
     @staticmethod
     def get_logger_level_by_str(str_level):
+        '''Convert string representation of level to python logging level
+
+        You can use this method to convert words 'CRITICAL', 'ERROR', 'WARNING',
+        'INFO', 'DEBUG', 'NOTSET' to appropriate logging levels.
+        You can use this in databases or config files
+        '''
         if str_level == 'CRITICAL':
             return logging.CRITICAL
         if str_level == 'ERROR':
@@ -68,11 +131,21 @@ class LoggerConfig():
 
     @classmethod
     def get_core_root_logger(cls):
+        '''Initialize root logger of library with name 'core'
+
+        Checks if custom_logger_enabled is True. 
+        In this case gets logger with name 'core' and returns. 
+        Otherwise initialize default logger with name 'core' and returns
+
+        If logging_enabled is False disables logger, even if it's not default logger
+
+        Sets level of the most root logger of whole app (getLogger() returns) to DEBUG
+        '''
         logging.getLogger().setLevel(logging.DEBUG)
 
         core_logger = logging.getLogger('core')
         if not cls.custom_logger_enabled:
-            cls.init_default_logger(core_logger)
+            cls._init_default_logger(core_logger)
 
         if cls.logging_enabled:
             logging.disable(logging.NOTSET)
@@ -82,5 +155,6 @@ class LoggerConfig():
         return core_logger
 
 def get_logger(name):
+    '''Returns sublogger of root 'core' logger with specific name'''
     core_root = LoggerConfig.get_core_root_logger()
     return core_root.getChild(name)
