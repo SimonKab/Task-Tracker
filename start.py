16538deae51b68ca90c2ce34ce2fbd4cfb4eb136
Simon Kab <sam24free@gmail.com>
2018-06-07 12:10:07 +0300

    Doc for start.py

--- start.py
+++ start.py
@@ -1,3 +1,9 @@
+'''Start point of console app
+
+When app will be installed, this script will be used as enter point in console app
+Run this script manually with python interpreter to launch console app without
+installing it
+'''
 import tasktracker_console.console_parser as parser
 
 def run():
